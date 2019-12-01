c5d12a8650d810cce46b633d5f35a23511eb5ce2
ltran <tranln025@gmail.com>
2019-11-30 20:37:02 -0800

    Initial commit. Navbar and About

---
+++ js/portfolio.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+  $('.sidenav').sidenav();
+  $('.tabs').tabs() // How to make work!!
+  $('.parallax').parallax();
+});
+
+const navbar = document.querySelector('nav');
+const navlink = document.getElementsByClassName('navlink');
+const toObserve = document.querySelector('.parallax-container');
+const options = {
+  rootMargin: "-100px 0px 0px 0px"
+};
+const observer = new IntersectionObserver(function(entries, observer) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      navbar.classList.add("nav-scrolled");
+      [...navlink].forEach(link => {
+        link.classList.add("navlink-scrolled");
+      })
+    } else {
+      navbar.classList.remove("nav-scrolled");
+      [...navlink].forEach(link => {
+        link.classList.remove("navlink-scrolled");
+      })
+    }
+  });
+}, options);
+
+observer.observe(toObserve);
+
+$(window).scroll(function(){
+  $('.banner-text').css('opacity', 1 - $(window).scrollTop() / 200);
+});
+
+
+
+
+
+
+
+
+
+// TODO 
+// Tabs on nav links
